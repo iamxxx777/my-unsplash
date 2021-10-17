de789472b222e7adebc413d46190777a53408d5a
iamxxx777 <iamxxx333@gmail.com>
2021-10-17 13:25:27 +0100

    fixing JSON error

--- components/Gridcolumn.js
+++ components/Gridcolumn.js
@@ -3,6 +3,17 @@ import columnStyles from "../styles/Gridcolumn.module.scss"
 
 const Gridcolumn = ({ data, del, url }) => {
 
+    data = data.replace(/\\n/g, "\\n")  
+               .replace(/\\'/g, "\\'")
+               .replace(/\\"/g, '\\"')
+               .replace(/\\&/g, "\\&")
+               .replace(/\\r/g, "\\r")
+               .replace(/\\t/g, "\\t")
+               .replace(/\\b/g, "\\b")
+               .replace(/\\f/g, "\\f");
+// remove non-printable and other non-valid JSON chars
+    data = data.replace(/[\u0000-\u0019]+/g,"");
+
     const images = JSON.parse(data);
 
     return (
