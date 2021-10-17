2aa3fcde86258ce3cc1c7c6dfa17a29a5b66ddbd
iamxxx777 <iamxxx333@gmail.com>
2021-10-17 13:55:09 +0100

    again

--- components/Gridcolumn.js
+++ components/Gridcolumn.js
@@ -3,18 +3,28 @@ import columnStyles from "../styles/Gridcolumn.module.scss"
 
 const Gridcolumn = ({ data, del, url }) => {
 
-    data = data.replace(/\\n/g, "\\n")  
+    data = data.replace(/\\r/g, "\\r").replace(/\\n/g, "\\n")  
                .replace(/\\'/g, "\\'")
                .replace(/\\"/g, '\\"')
                .replace(/\\&/g, "\\&")
-               .replace(/\\r/g, "\\r")
                .replace(/\\t/g, "\\t")
                .replace(/\\b/g, "\\b")
-               .replace(/\\f/g, "\\f");
+               .replace(/\\f/g, "\\f")
+               .replace(/\\ro/g, "\\ro")
+               .replace(/\\jo/g, "\\jo")
+               .replace(/\\ro/g, "\\ro")
+               .replace(/\\Hu/g, "\\Hu")
+               .replace(/\\Pi/g, "\\Pi")
+               .replace(/\\xi/g, "\\xi")
+               .replace(/\\_i/g, "\\_i")
+               .replace(/\\vi/g, "\\vi");
 // remove non-printable and other non-valid JSON chars
     data = data.replace(/[\u0000-\u0019]+/g,"");
 
     const images = JSON.parse(data);
+
+    console.log(data);
+    console.log(images);
 
     return (
         <div className={columnStyles.grid_column}>
